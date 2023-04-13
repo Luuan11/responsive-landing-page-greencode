4c4f084e375ab655240c88adf85f29c53be499ba
Luan Fernando <luancraft55@gmail.com>
2023-04-13 10:46:57 -0300

    :sparkles: created the scroll transition

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,3 +21,13 @@ const linkAction = () =>{
     navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+
+const bgHeader = () =>{
+    const header = document.getElementById('header');
+    this.scrollY >= 50 ? header.classList.add('bg-header')
+                       : header.classList.remove('bg-header')
+}
+
+window.addEventListener('scroll', bgHeader)
