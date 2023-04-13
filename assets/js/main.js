804409244a5a8741b6a5df2110c1fbc53b34b18b
Luan Fernando <luancraft55@gmail.com>
2023-04-13 20:52:19 -0300

    :sparkles: added the functions on main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,3 +31,30 @@ const bgHeader = () =>{
 }
 
 window.addEventListener('scroll', bgHeader)
+
+const scrollUp = () =>{
+    const scrollUp = document.getElementById('scroll-up')
+    this.scroll >= 350 ? scrollUp.classList.add('show-scroll')
+                       : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () =>{
+  	const scrollY = window.pageYOffset
+
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+		if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link')
+		}else{
+			sectionsClass.classList.remove('active-link')
+		}                                                    
+	})
+}
+window.addEventListener('scroll', scrollActive)
